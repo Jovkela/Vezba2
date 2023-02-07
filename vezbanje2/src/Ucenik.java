4a5449531bec2be74b0af62ea6fa77576c83174e
Jovkela <123483811+Jovkela@users.noreply.github.com>
2023-02-07 11:07:41 +0100

    Add files via upload

---
+++ vezbanje2/src/Ucenik.java
@@ -0,0 +1,9 @@
+package org.etsntesla.arm.osobe;
+public class Ucenik extends Osoba{
+    string smer
+            int god_pohadjanja
+                    public Ucenik(String ime,int god_pohadjanja)
+                        public Ucenik (String ime,int god){
+
+                        }
+}
